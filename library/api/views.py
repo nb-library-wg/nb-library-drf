4c5a1673bdfa363dd58503256abe6f4c5ebc55a0
James Addison <jay@jp-hosting.net>
2017-08-15 21:03:46 -0700

    Add books endpoint

--- library/api/views.py
+++ library/api/views.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from library.api.models import Book
 from library.api.serializers import UserSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Book.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
--- library/urls.py
+++ library/urls.py
@@ -4,6 +4,7 @@ from library.api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'books', views.BookViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
